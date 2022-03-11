a845fca615116dfc40b191abeb9fd6a634e59eba
Danilo Jimenez <45516306+DaniloJ0@users.noreply.github.com>
2022-03-10 19:50:18 -0500

    Modelos organizados

--- characters.js
+++ characters.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
-
+const data = require('./data')
 //Get
+router.get('/', async (req,res)=>{
+        res.status(200).json(data.characters)
+});
 
 //Post 
 
