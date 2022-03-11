3f841f9d22923cac0bab96322f5074e601d59e33
Danilo Jimenez <45516306+DaniloJ0@users.noreply.github.com>
2022-03-10 19:21:52 -0500

    Models Created

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express= require('express')
-const res = require('express/lib/response');
+const res = require('express/lib/response')
 
-const app = express();
-app.use(express.json());
-app.listen(3000,()=>{
-    console.log('This server is running');
-});
+const app = express()
+
+app.use(express.json())
+
+app.listen(3000,()=>{console.log('This server is running');})
+
+
+const characters_stats = require('./characters_stats')
+const characters = require('./characters')
+const images_2d= require('./images_2d')
+const items = require('./routes/items')
+const mission_objectives = require('./routes/mission_objectives')
+const missions= require('./routes/missions')
+const models_3d = require('./routes/models_3d')
+const player_characters = require('./routes/player_characters')
+const players = require('./routes/players')
+
+app.use('/truynob', characters_stats)
+app.use('/truynob', characters)
+app.use('/truynob', images_2d)
+app.use('/truynob', items)
+app.use('/truynob', mission_objectives)
+app.use('/truynob', missions)
+app.use('/truynob', models_3d)
+app.use('/truynob', player_characters)
+app.use('/truynob', players)
+
+
+
 
 
 
---
+++ models_3d.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+//Get
+
+//Post 
+
+// Delete
+
+
+//Patch
+
+
+module.exports = router;
